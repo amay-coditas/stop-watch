3437e91d71f14935c492a2ab2565c8b4aa0eea32
Amay Sandanshiv <129855154+amay-coditas@users.noreply.github.com>
2025-03-29 03:15:37 +0000

    added css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import { useRef, useState } from "react";
 export default function App() {
   const [timer, setTimer] = useState(0);
   const ref = useRef();
-  const [timeStatus, setTimeStatus] = useState("stop");
+  const [isTimerRuning, setTimerRuning] = useState(false);
 
-  const onStartClick = () => {
-    setTimeStatus("start");
+  const startTimer = () => {
+    setTimerRuning(true);
     clearInterval(ref.current);
     ref.current = setInterval(() => {
       setTimer((prev) => prev + 1);
     }, 1000);
   };
 
-  const onStop = () => {
-    setTimeStatus("stop");
+  const stopTimer = () => {
+    setTimerRuning(false);
     clearInterval(ref.current);
+  };
+
+  const onButtonClick = () => {
+    if (isTimerRuning) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
   };
 
   const hours = Math.trunc(timer / (60 * 60));
@@ -25,18 +33,19 @@ export default function App() {
 
   return (
     <div className="App">
-      <div className="top-right-text">
-        Created and Hosted by Amay Sandanshiv
-      </div>
-      <div>Stop watch {timeStatus}</div>
-      <div className="timer-container">
-        <span>{String(hours).padStart(2, "0")}</span> :
-        <span>{String(minutes).padStart(2, "0")}</span> :
-        <span>{String(seconds).padStart(2, "0")}</span>
-      </div>
-      <div className="button-container">
-        <button onClick={onStartClick}>Start</button>
-        <button onClick={onStop}>Stop</button>
+      <div className="top-right-text">Author Amay Sandanshiv</div>
+      <div className="flex-container">
+        <div>Stop watch</div>
+        <div className="timer-container">
+          <span>{String(hours).padStart(2, "0")}</span> :
+          <span>{String(minutes).padStart(2, "0")}</span> :
+          <span>{String(seconds).padStart(2, "0")}</span>
+        </div>
+        <div className="button-container">
+          <button onClick={onButtonClick}>
+            {isTimerRuning ? "Stop" : "Start"}
+          </button>
+        </div>
       </div>
     </div>
   );
